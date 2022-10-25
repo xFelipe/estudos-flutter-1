359b7efacdca44a1b10ecd663f109b0ceb21126f
Felipe Gomes Duarte <felipe.duarte@cortex-intelligence.com>
2022-10-24 23:39:43 -0300

    Bordas e ícones em quadrados do centro.

--- lib/main.dart
+++ lib/main.dart
@@ -18,37 +18,9 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
         appBar: AppBar(title: Text("Flutter: Meus primeiros passos")),
-        body:
-            Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-          Row(
-            children: [
-              RandomColorRectangle(),
-              RandomColorRectangle(),
-              RandomColorRectangle(),
-            ],
-          ),
-          Row(
-            children: [
-              RandomColorRectangle(),
-              RandomColorRectangle(),
-              RandomColorRectangle(),
-            ],
-          ),
-          Row(
-            children: [
-              RandomColorRectangle(),
-              RandomColorRectangle(),
-              RandomColorRectangle(),
-            ],
-          ),
-          Row(
-            children: [
-              RandomColorRectangle(),
-              RandomColorRectangle(),
-              RandomColorRectangle(),
-            ],
-          ),
-        ]),
+        body: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: const [ColorsRow(), ColorsRow(), ColorsRow(), ColorsRow()]),
       ),
     );
   }
@@ -56,6 +28,7 @@ class MyApp extends StatelessWidget {
 
 class RandomColorRectangle extends StatefulWidget {
   Color color = Colors.white;
+  Widget? child;
 
   static Color _randomColor() {
     return Color((math.Random().nextDouble() * 0xFFFFFF).toInt())
@@ -66,7 +39,7 @@ class RandomColorRectangle extends StatefulWidget {
     color = _randomColor();
   }
 
-  RandomColorRectangle({Key? key}) : super(key: key) {
+  RandomColorRectangle({Key? key, this.child}) : super(key: key) {
     resetColor();
   }
 
@@ -78,14 +51,36 @@ class _RandomColorRectangleState extends State<RandomColorRectangle> {
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () { setState(() {
-        widget.resetColor();
-      });},
+      onTap: () {
+        setState(() {
+          widget.resetColor();
+        });
+      },
       child: Container(
-        color: widget.color,
+        decoration: BoxDecoration(
+            color: widget.color,
+            borderRadius: BorderRadius.circular(10),
+            border: Border.all(width: 4)),
         height: 150,
-        width: 130,
+        width: 125,
+        child: widget.child
       ),
+    );
+  }
+}
+
+class ColorsRow extends StatelessWidget {
+  const ColorsRow({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        RandomColorRectangle(),
+        RandomColorRectangle(child: const Icon(Icons.people_alt)),
+        RandomColorRectangle(),
+      ],
     );
   }
 }
